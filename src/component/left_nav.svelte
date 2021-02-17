6deb25700946a3be7bf552825a8a25a037a597fd
Ganda Wenang Sani <wenang.sani@gmail.com>
2021-02-17 08:44:37 +0800

    add menu belum tersedia

--- src/component/left_nav.svelte
+++ src/component/left_nav.svelte
@@ -1,21 +1,27 @@
+<script>
+  function domenu() {
+    alert("Menu belum tersedia...");
+  }
+</script>
+
 <nav class="list-group">
   <a href="#!" class="list-group-item list-group-item-action">KRITERIA 1: </a>
-  <a href="#!" class="list-group-item list-group-item-action">KRITERIA 2: </a>
-  <a href="#!" class="list-group-item list-group-item-action">KRITERIA 3: </a>
-  <a href="#!" class="list-group-item list-group-item-action">KRITERIA 4: </a>
-  <a href="#!" class="list-group-item list-group-item-action"
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 2: </a>
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 3: </a>
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 4: </a>
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action"
     >KRITERIA 5: KEUANGAN, SARANA, DAN PRASARANA
   </a>
-  <a href="#!" class="list-group-item list-group-item-action"
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action"
     >KRITERIA 6: KURIKULUM
   </a>
-  <a href="#!" class="list-group-item list-group-item-action"
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action"
     >KRITERIA 7: PENELITIAN
   </a>
-  <a href="#!" class="list-group-item list-group-item-action"
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action"
     >KRITERIA 8: PENGABDIAN KEPADA MASYARAKAT
   </a>
-  <a href="#!" class="list-group-item list-group-item-action"
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action"
     >KRITERIA 9: LUARAN DAN CAPAIAN TRIDHARMA
   </a>
 </nav>
