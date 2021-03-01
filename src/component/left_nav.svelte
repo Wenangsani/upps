9fa4158c1798041790c98c229d72194177d3bbcb
Ganda Wenang Sani <wenang.sani@gmail.com>
2021-03-01 10:23:27 +0800

    new update

--- src/component/left_nav.svelte
+++ src/component/left_nav.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <nav class="list-group">
-  <a href="#!" class="list-group-item list-group-item-action">KRITERIA 1: </a>
-  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 2: </a>
-  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 3: </a>
-  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 4: </a>
+  <a href="#!" class="list-group-item list-group-item-action">KRITERIA 1: VISI, MISI, TUJUAN DAN STRATEGI</a>
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 2: TATA PAMONG DAN TATA KELOLA</a>
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 3: MAHASISWA</a>
+  <a href="#!" on:click={domenu} class="list-group-item list-group-item-action">KRITERIA 4: SUMBER DAYA</a>
   <a href="#!" on:click={domenu} class="list-group-item list-group-item-action"
     >KRITERIA 5: KEUANGAN, SARANA, DAN PRASARANA
   </a>
