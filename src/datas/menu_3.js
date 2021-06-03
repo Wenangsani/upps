a91460eeb231e004455a6c0faebcf676668256bd
ganda wenang sani <wenang.sani@gmail.com>
2021-06-03 22:12:10 +0800

    add menu 3

--- src/datas/menu_3.js
+++ src/datas/menu_3.js
@@ -1,3 +1,167 @@
 export default [
-  ];
-  
+  {
+    No: "1.a",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Koordinator Prodi di bawah Rektor, Wakil Dekan/Direktur bidang akademik, Wakil Dekan / Direktur bidang kemahasiswaan, GPM",
+    Dokumen: [
+      {
+        Name: "Standar mutu bidang kemahasiswaan UPPS yang memiliki legalitas formal",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.b",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS",
+    Dokumen: [
+      {
+        Name: "Program kerja terkait peningkatan prestasi akademik dan non akademik mahasiswa",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.c",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS",
+    Dokumen: [
+      {
+        Name: "Program kerja terkait peningkatan Luaran Penelitian dan Pengmas mahasiswa",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.d",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS",
+    Dokumen: [
+      {
+        Name: "Program kerja terkait peningkatan layanan kepada mahasiswa",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.e",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS",
+    Dokumen: [
+      {
+        Name: "Program kerja terkait tracer study dan alumni",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.f",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang akademik, Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS",
+    Dokumen: [
+      {
+        Name: "Program Kerja terkait mahasiswa internasional",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.g",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang akademik, Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS",
+    Dokumen: [
+      {
+        Name: "Program kerja terkait peningkatan kualitas dan kuantitas mahasiswa baru",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.h",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP Penerimaan mahasiswa baru",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.i",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP terkait mahasiswa internasional",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.j",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP terkait Student body",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.k",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP terkait yudisium dan wisuda",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.l",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP terkait beasiswa",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.m",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP Tracer study",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.n",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP terkait kegiatan kemahasiswaan dan layanan kepada mahasiswa",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "1.o",
+    Lingkup: "Standar Mutu, Pedoman dan Program Kerja terkait mahasiswa",
+    Auditee: "Wakil Dekan/Direktur bidang kemahasiswaan, Koordinator Prodi di bawah Rektor, kasubag akademik dan kemahasiswaan, Ka PS, GPM",
+    Dokumen: [
+      {
+        Name: "SOP terkait mahasiswa internasional",
+      },
+    ],
+    Catatan: [],
+  },
+];
