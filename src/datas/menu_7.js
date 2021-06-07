a1f18e207d1d1459d307b60622224dd49e8751b9
Wenangsani <wenang.sani@gmail.com>
2021-06-07 02:13:18 -0700

    update menu

--- src/datas/menu_7.js
+++ src/datas/menu_7.js
@@ -1,4 +1,193 @@
 export default [
-    
-  ];
-  
+  {
+    No: "1",
+    Lingkup: "Standar Mutu Terkait Penelitian",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik",
+    Dokumen: [
+      {
+        Name: "Standar SPMI terkait Penelitian UPPS yang memiliki legalitas Formal",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "2",
+    Lingkup: "Renstra UPPS Pilar Penelitian",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik, BP3F",
+    Dokumen: [
+      {
+        Name: "Renstra UPPS Pilar Penelitian yang memiliki legalitas formal",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "3",
+    Lingkup: "Renop UPPS Pilar Penelitian",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik, BP3F",
+    Dokumen: [
+      {
+        Name: "Renop UPPS Pilar Penelitian yang memiliki legalitas formal",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "4",
+    Lingkup: "RIP",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "SK Tim Pelaksana/penyusun",
+      },
+      {
+        Name: "SOP",
+      },
+      {
+        Name: "kinerja tim",
+      },
+      {
+        Name: "workshopp dengan keterlibatan dari : Pemangku kepentingan internal, eksternal dan pakar sesuaik bidang ilmu PS",
+      },
+      {
+        Name: "RKF dengan agenda pembahasan kurikulum hasil workshop",
+      },
+      {
+        Name: "Rapat senat untuk pemberian rekomendasi pengesahan kurikulum",
+      },
+      {
+        Name: "SK pengesahan",
+      },
+      {
+        Name: "Laporan Lengkap Proses Penyusunan",
+      },
+      {
+        Name: "RIP UPPS yang memiliki legalitas Formal",
+      },
+    ],
+    Catatan: ["Dokumen bukti dijadikan 1 berkas"],
+  },
+  {
+    No: "5",
+    Lingkup: "Program Kerja BP3F terkiat Penelitian",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Program kerja BP3F bidang penelitian yang memiliki legalitas Formal (3 tahun) disertai SOP",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "6",
+    Lingkup: "Pedoman-Pedoman Penelitian dari berbagai sumber anggaran",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Pedoman-pedoman penelitian dari berbagai sumber anggaran",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "7",
+    Lingkup: "Organisasi penelitian",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "SK Struktur Organisasi",
+      },
+      {
+        Name: "SK Pengurus",
+      },
+      {
+        Name: "Kejelasan tupoksi (disahkan)",
+      },
+      {
+        Name: "SDM yang memenuhi kulalifikasi dan jumlah yang memadai",
+      },
+      {
+        Name: "Sarana dan prasarana yang mendukung",
+      },
+    ],
+    Catatan: ["Dokumen bukti setiap tahun dijadikan 1 berkas"],
+  },
+  {
+    No: "8",
+    Lingkup: "Program kerja UPPS Peningkatan Publikasi dan Luaran Penelitian ",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik, BP3F",
+    Dokumen: [
+      {
+        Name: "Program kerja UPPS Peningkatan Publikasi Penelitian Jurnal Terakrediasi Nasional dan Internasional dan atau prosiding nasional dan internasional bagi dosen dan mahasiswa",
+      },
+      {
+        Name: "Program kerja UPPS Peningkatan luaran penelitian bagi dosen dan mahasiswa (HaKI, Paten, Buku Ajar dll)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "9",
+    Lingkup:
+      "SOP Publikasi, HakI, teknologi Tepat Guna, Produk (Produk Terstandarisasi, Produk Tersertifikasi), Karya Seni, Rekayasa Sosial dll terkait luaran penelitian",
+    Dokumen: [
+      {
+        Name: "SOP pengarsipan Penelitian Dosen dari berbagai sumber anggaran",
+      },
+      {
+        Name: "SOP pengarsipan publikasi Dosen dan mahasiswa ",
+      },
+      {
+        Name: "SOP pengarsipan Buku ajar, dll Dosen dan mahasiswa ",
+      },
+      {
+        Name: "SOP pengarsipan HaKI Dosen dan mahasiswa ",
+      },
+      {
+        Name: "SOP Lain terkiat Luaran Penelitian dosen dan mahasiswa",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "10",
+    Lingkup:
+      "Sosialisasi kebijakan, standar mutu, pedoman dll terkait penelitian",
+    Auditee:
+      "Wakil Dekan/Direktur bidang akademik, Koordinator Prodi di bawah Rektro, BP3F",
+    Dokumen: [
+      {
+        Name: "SOP",
+      },
+      {
+        Name: "TOR",
+      },
+      {
+        Name: "Bukti kinerja tim",
+      },
+      {
+        Name: "Laporan lengkap",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+];
--- src/datas/menu_8.js
+++ src/datas/menu_8.js
@@ -1,3 +1,258 @@
 export default [
-    
+  {
+    No: "1",
+    Lingkup: "Standar Mutu Terkait Pengabdian kepada masyarakat",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik",
+    Dokumen: [
+      {
+        Name: "Standar Mutu terkait Pengabdian kepada masyarakat UPPS",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "2",
+    Lingkup: "Renstra UPPS Pilar Pengmas",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik, BP3F",
+    Dokumen: [
+      {
+        Name: "Renstra UPPS Pilar Pengmas yang memiliki legalitas formal",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "3",
+    Lingkup: "Renop UPPS Pilar Pengmas",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik, BP3F",
+    Dokumen: [
+      {
+        Name: "Renop UPPS Pilar Pengmas yang memiliki legalitas formal",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "4",
+    Lingkup: "RIPengmas",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "SK Tim Pelaksana/penyusun",
+      },
+      {
+        Name: "SOP",
+      },
+      {
+        Name: "kinerja tim",
+      },
+      {
+        Name: "workshopp dengan keterlibatan dari : Pemangku kepentingan internal, eksternal dan pakar sesuaik bidang ilmu PS",
+      },
+      {
+        Name: "RKU/RKF dengan agenda pembahasan kurikulum hasil workshop",
+      },
+      {
+        Name: "Rapat senat untuk pemberian rekomendasi pengesahan kurikulum",
+      },
+      {
+        Name: "SK pengesahan",
+      },
+      {
+        Name: "Laporan Lengkap",
+      },
+    ],
+    Catatan: ["Dokumen bukti dijadikan 1 berkas"],
+  },
+
+  {
+    No: "5",
+    Lingkup: "Program Kerja",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Program kerja BP3F bidang pengmas yang memiliki legalitas Formal (3 tahun) disertai SOP",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "6",
+    Lingkup:
+      "Pedoman-Pedoman Pengabdian kepada masyarakat dari berbagai sumber anggaran",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Pedoman-pedoman Pengabdian kepada masyarakat dari berbagai sumber anggaran",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "7",
+    Lingkup: "Organisasi Pengabdian kepada masyarakat",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "SK Struktur Organisasi",
+      },
+      {
+        Name: "SK Pengurus",
+      },
+      {
+        Name: "Kejelasan tupoksi (disahkan)",
+      },
+      {
+        Name: "SDM yang memenuhi kulalifikasi dan jumlah yang memadai",
+      },
+      {
+        Name: "Sarana dan prasarana yang mendukung",
+      },
+    ],
+    Catatan: ["Dokumen bukti setiap tahun dijadikan 1 berkas"],
+  },
+  {
+    No: "8",
+    Lingkup: "Program kerja UPPS Peningkatan Publikasi Pengmas",
+    Auditee:
+      "Dekan/Direktur/Koordinator Prodi di bawah Rektor/Wakil Dekan/Direktur Bidang Akademik, BP3F",
+    Dokumen: [
+      {
+        Name: "Program kerja UPPS Peningkatan Publikasi Pengmas Jurnal Terakrediasi Nasional dan Internasional dan atau prosiding nasional dan internasional bagi dosen dan mahasiswa",
+      },
+      {
+        Name: "Program kerja UPPS Peningkatan luaran pengmas bagi dosen dan mahasiswa (HaKI, Paten, Buku Ajar dll)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "9",
+    Lingkup: "SOP dokumentasi Lyaran Pengmas  ",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "SOP pengarsipan Pengabdian kepada masyarakat Dosen dari berbagai sumber anggaran",
+      },
+      {
+        Name: "SOP pengarsipan publikasi Dosen dan mahasiswa terkait pengmas ",
+      },
+      {
+        Name: "SOP Lain terkiat peningkatan Publikasi dan Luaran/Outcome Pengmas",
+      },
+    ],
+    Catatan: [],
+  },
+  {
+    No: "10",
+    Lingkup:
+      "Sosialisasi kebijakan, standar mutu, pedoman dll terkait penelitian",
+    Auditee:
+      "Wakil Dekan/Direktur bidang akademik, Koordinator Prodi di bawah Rektor, BP3F",
+    Dokumen: [
+      {
+        Name: "Sosialisasi kebjakan, standar mutu, pedoman dll terkait penelitian dalam 3 tahun terakhir",
+      },
+      {
+        Name: "SOP",
+      },
+      {
+        Name: "TOR",
+      },
+      {
+        Name: "Bukti kinerja tim",
+      },
+      {
+        Name: "Laporan lengkap",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "11",
+    Lingkup: "Data Pengabdian Kepada Masyarakat Dosen Tetap",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Rekapitulasi Data pengmas dosen tetap dari berbagai sumber anggaran beserta besaran anggaran (3 tahun terakhir)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "12",
+    Lingkup: "Data Pengabdian Kepada Masyarakat Dosen tidak tetap",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Rekapitulasi Data pengmas dosen tidak tetap dari berbagai sumber anggaran beserta besaran anggaran (3 tahun terakhir)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "13",
+    Lingkup: "Data Publikasi Pengabdian Kepada Masyarakat Dosen Tetap",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Rekapitulasi Data Publikasi Pengmas dosen tetap (nama dosen, judul publikasi, nama jurnal/seminra, kriteria Jurnal/seminar (3 tahun terakhir)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "14",
+    Lingkup: "Data Publikasi Pengabdian Kepada Masyarakat Dosen tidak Tetap",
+    Auditee: "BP3F",
+    Dokumen: [
+      {
+        Name: "Rekapitulasi Data Publikasi Pengmas dosen tidak tetap (nama dosen, judul publikasi, nama jurnal/seminar, kriteria Jurnal/seminar (3 tahun terakhir)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
+  {
+    No: "15",
+    Lingkup:
+      "Pengabdian kepada masyarakat dosen yang melibatkan mahasiswa (judul pengmas dosen, nama mahasiswa, judul pengmas mahasiswa)",
+    Auditee:
+      "Koordinator Prodi bawah Rek, Semua Wakil  Dekan/Direktur Bidang, Ka TU,  Kasubag Keuangan dan Umum, BP3F,  Ka PS",
+    Dokumen: [
+      {
+        Name: "Rekapitulasi / Data Pengabdian kepada masyarakat dosen yang melibatkan mahasiswa (judul pengmas dosen, nama mahasiswa, judul pengmas mahasiswa)",
+      },
+    ],
+    Catatan: [
+      "Minimal tersedia program kerja 3 tahun 2017-2019",
+      "Dokumen bukti setiap tahun dijadikan 1 berkas",
+    ],
+  },
 ];
